285a7590c9b8c11e6cbc929dea555556d790c0cb
grzegorczykdev <grzegorczykdev@gmail.com>
2025-10-23 22:20:46 +0200

    Refactor App component to include a structured layout with header and search prompt

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main>
+      <div className="pattern"></div>
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Your Next Favorite</span>{" "}
+            Movies You'll Enjoy Without the Hassle
+          </h1>
+        </header>
+        <p>Search</p>
+      </div>
+    </main>
   );
 };
 
